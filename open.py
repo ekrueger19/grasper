9424e255e3d5263149b02823ebf4ee9b059ad8be
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-09-04 13:26:42 -0500

    this is going to open and close the grasper

---
+++ open.py
@@ -0,0 +1,6 @@
+import setup
+import RoboPiLib as RPL
+import time
+
+#this should open and close the grasper based on input commands
+#we should do it based on time, which means we need a max time so it won't break
