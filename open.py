6e307a8f924440e80e88cf3201a426dd75619ad0
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-09-21 13:49:39 -0500

    changed stuff

--- open.py
+++ open.py
@@ -2,19 +2,20 @@ import setup
 import RoboPiLib as RPL
 import time
 
+w = raw_input("> ")
 x = 1
 y = 2000
 z = 1000
 
 print "Enter a to open and b to close: "
-if raw_input("> ") == "a":
-    T = time.time() + 1
+if w == "a":
+    T = time.time() + 2
     while time.time() < T:
         RPL.servoWrite(x, y)
         if time.time() >= T:
             RPL.servoWrite(x, 0)
-elif raw_input("> ") == "b":
-    T = time.time() + 1
+elif w == "b":
+    T = time.time() + 2
     while time.time() < T:
         RPL.servoWrite(x, z)
         if time.time() >= T:
