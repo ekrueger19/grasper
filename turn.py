20015205eb31834d6656703c39bd3d70da6137d1
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-09-24 13:42:31 -0500

    hopefully this works

--- turn.py
+++ turn.py
@@ -2,4 +2,13 @@ import setup
 import RoboPiLib as RPL
 import time
 
-x = 2
+x = 0
+start = 600
+end = 3000
+
+RPL.servoWrite(0, start)
+print "press a to turn right and b to turn left"
+while raw_input("> ") == "a":
+    RPL.servoWrite(0, end)
+    if raw_input("> ") =! "a":
+        RPL.servoWrite(0, start)
