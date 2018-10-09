2a0ef23c9a7327214b96aa0a24f8e316a9e85021
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-10-09 13:52:05 -0500

    ugh

--- multi.py
+++ multi.py
@@ -5,3 +5,10 @@ import time
 x = 1
 y = 2
 twist = 0
+
+print "h controls opening/closing"
+print "v controls up and down"
+print "z controls twisting"
+def op(h,v,z):
+    if h = 1:
+        RPL.servoWite(1, )
