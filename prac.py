0d740b32c1144815fb016ab1cb640b82d00f53ec
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-10-24 13:26:18 -0500

    fixed some stuff

---
+++ prac.py
@@ -0,0 +1,41 @@
+import time
+#this is a test of whether multi would work
+print "h controls opening"
+print "v controls up"
+print "q controls down"
+print "motor controls twisting"
+def op(h):
+    L = 0.5 + h - 1
+    L >= 0
+    L < 5.6 #placeholder maximum
+    T = time.time() + L
+    while time.time() < T:
+        print "opening"
+        if time.time() >= T:
+            print "stopping"
+def up(v):
+    P = 0.5 + v - 1
+    0 =< P < 5.6 #placeholder maximum
+    A = time.time() + P
+    while time.time() < A:
+        print "going up"
+        if time.time() >= A:
+            print "stopping"
+def do(q):
+    B = 0.5 + q - 1
+    0 =< B < 5.6 #placeholder maximum
+    C = time.time() + B
+    while time.time() < C:
+        print "going down"
+        if time.time() >= C:
+            print "stopping"
+def tw(motor):
+    if motor == 3000:
+        print "turn left"
+    elif motor == 600:
+        print "turn right"
+
+h = raw_input("h: ")
+v = raw_input("v: ")
+q = raw_input("q: ")
+motor = raw_input("motor: ")
