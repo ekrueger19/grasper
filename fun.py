be6027ffea990b00fd2973980dc56996b283ee9d
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-10-24 13:50:53 -0500

    alsd;kfak

---
+++ fun.py
@@ -0,0 +1,13 @@
+import time
+#this code should take a string and make it an action
+
+x = 1
+y = 2
+twist = 0
+
+print "What do you want to do? Write then between each action."
+print "Actions include: up, down, close, open, left-twist, right-twist"
+def act(w):
+    return list(w)
+w = raw_input("> ")
+act(w)
