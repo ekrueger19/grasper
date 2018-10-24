aeba7f6a9186f8b0c4a3055ae572ef200c575d62
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-10-24 13:13:09 -0500

    changes

--- multi.py
+++ multi.py
@@ -6,9 +6,42 @@ x = 1
 y = 2
 twist = 0
 
-print "x controls opening/closing"
-print "y controls up and down"
-print "twist controls twisting"
-def op(h,v,z):
-    if h = 1:
-        RPL.servoWite(1, )
+print "h controls opening/closing"
+print "v controls up"
+print "q controls down"
+print "motor controls twisting"
+def op(h):
+    L = 0.5 + h - 1
+    L < 5.6 #placeholder maximum
+    T = time.time() + L
+    while time.time() < T:
+        RPL.servoWrite(x, 2000)
+        if time.time() >= T:
+            RPL.servoWrite(x, 0)
+def up(v):
+    P = 0.5 + v - 1
+    P < 5.6 #placeholder maximum
+    A = time.time() + P
+    while time.time() < A:
+        RPL.servoWrite(y, 2000)
+        if time.time() >= A:
+            RPL.servoWrite(y, 0)
+def do(q):
+    B = 0.5 + q - 1
+    B < 5.6 #placeholder maximum
+    C = time.time() + B
+    while time.time() < C:
+        RPL.servoWrite(y, 1000)
+        if time.time() >= C:
+            RPL.servoWrite(y, 0)
+def tw(motor):
+    motor = servoRead(0)
+    if motor == 3000:
+        RPL.servoWrite(twist, 600)
+    elif motor == 600:
+        RPL.servoWrite(twist, 3000)
+
+h = raw_input("h: ")
+v = raw_input("v: ")
+q = raw_input("q: ")
+motor = raw_input("motor: ")
