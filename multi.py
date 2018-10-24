2112aac8adc393f3892ed7d9b92cc63dbe25deb5
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-10-24 13:27:53 -0500

    idk

--- multi.py
+++ multi.py
@@ -12,6 +12,7 @@ print "q controls down"
 print "motor controls twisting"
 def op(h):
     L = 0.5 + h - 1
+    L >= 0
     L < 5.6 #placeholder maximum
     T = time.time() + L
     while time.time() < T:
@@ -20,6 +21,7 @@ def op(h):
             RPL.servoWrite(x, 0)
 def up(v):
     P = 0.5 + v - 1
+    P >= 0
     P < 5.6 #placeholder maximum
     A = time.time() + P
     while time.time() < A:
@@ -28,6 +30,7 @@ def up(v):
             RPL.servoWrite(y, 0)
 def do(q):
     B = 0.5 + q - 1
+    B >= 0
     B < 5.6 #placeholder maximum
     C = time.time() + B
     while time.time() < C:
