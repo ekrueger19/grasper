f0a7a4d48ef3a5d9484a61ce75ef13f9f24c17d4
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-11-26 08:49:42 -0600

    timebase

---
+++ timebase.py
@@ -0,0 +1,12 @@
+import time
+import setup
+import RoboPiLib as RPL
+
+x = 1
+y = 2
+twist = 0
+
+print "POSITIVE numbers mean up, NEGATIVE numbers mean down"
+print "POSITIVE numbers mean open, NEGATIVE numbers mean close"
+print "POSITIVE numbers mean right, NEGATIVE numbers mean left"
+print "The further from zero, the more it moves"
